8577d6d667420f30783b9c471d3531fdce68af69
masad26 <sheikhasad2662@gmail.com>
2020-10-02 17:58:25 +0500

    Source Code

---
+++ WebviewLandscape/app/src/main/java/com/sahi/webviewlandscape/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sahi.webviewlandscape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.clearHistory();
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.loadUrl("https://edition.cnn.com/");
+    }
+}
